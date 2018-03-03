b6c4ab09fc8626c28d5c39b1bcffbe1d677eddd4
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-03-03 16:32:18 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,56 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season,contestants|
+    contestants.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  i = 0
+  data.each do |season,contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        i +=1
+      end
+    end
+  end
+  return i
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |season,contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  avg = 0
+  counter = 0
+  data[season].each do |contestant|
+    avg += contestant["age"].to_f
+    counter += 1
+  end
+  return (avg/counter).round
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require_relative "../lib/bachelor.rb"
+require 'pry'
 require 'json'
-  
+
+
 RSpec.configure do |config|
 
 end
