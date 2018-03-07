b30f5031f512d9cb1073f81a71c9fd6a031c01bb
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-03-07 01:32:10 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,6 +3,7 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   # code here
   data[season].each do |contestant|
+
     if contestant["status"] == "Winner"
       return contestant["name"].split(" ")[0]
     end
